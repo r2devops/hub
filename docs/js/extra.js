974f423ff422751a5d769e28c4c5b7080f8770ff
Protocole <burgoni.alexandre@protonmail.com>
2020-11-05 12:51:03 +0100

    Fix Concept Button position

---
+++ docs/js/extra.js
@@ -0,0 +1,3 @@
+var $buttons = document.getElementsByClassName("md-button-center");
+for (let i = 0; i < $buttons.length; i++) 
+    $buttons[i].parentNode.setAttribute("style", "text-align:center");   
