5221dbe0efcd0322b5633f9d8976e958fd8ccdd6
Protocole <burgoni.alexandre@protonmail.com>
2020-11-05 15:34:26 +0100

    Update Concept, Rm Bounce & fix button

--- docs/js/extra.js
+++ docs/js/extra.js
@@ -1,3 +1,5 @@
 var $buttons = document.getElementsByClassName("md-button-center");
-for (let i = 0; i < $buttons.length; i++) 
-    $buttons[i].parentNode.setAttribute("style", "text-align:center");   
+for (let i = 0; i < $buttons.length; i++)  {
+    $buttons[i].parentNode.setAttribute("style", "text-align:center;");   
+    $buttons[i].setAttribute("style", "display: inline-block");   
+}
