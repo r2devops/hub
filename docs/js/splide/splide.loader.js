f4ab96a62e53243a6a307b499a15bbf21a33149b
Alexandre Burgoni <alexandre@go2scale.com>
2020-12-22 09:59:21 +0100

    add Splide script loader

--- docs/js/splide/splide.loader.js
+++ docs/js/splide/splide.loader.js
@@ -1,4 +1,11 @@
 function initSplide() {
     console.log("Loading splide");
-    new Splide( '.splide' ).mount();
+    new Splide( '.splide', {
+        autoplay: true,
+        interval: 5000,
+        pauseOnHover: true,
+        resetProgress: true,
+        rewind: true,
+        lazyLoad: 'nearby'
+    } ).mount();
 }
