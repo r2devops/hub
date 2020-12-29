1875c37d080f9a8b03b07d5aeffaf24c464b1cd1
Alexandre Burgoni <alexandre@go2scale.com>
2020-12-29 11:31:00 +0100

    Setting Gallery as slide with thumbnails

--- docs/js/splide/splide.loader.js
+++ docs/js/splide/splide.loader.js
@@ -1,11 +1,31 @@
 function initSplide() {
     console.log("Loading splide");
-    new Splide( '.splide', {
+	var secondarySlider = new Splide( '#secondary-slider', {
+		fixedWidth  : 100,
+		height      : 60,
+		gap         : 10,
+		cover       : true,
+		isNavigation: true,
+		focus       : 'center',
+		breakpoints : {
+			'600': {
+				fixedWidth: 66,
+				height    : 40,
+			}
+        },
         autoplay: true,
         interval: 5000,
         pauseOnHover: true,
         resetProgress: true,
         rewind: true,
-        lazyLoad: 'nearby'
-    } ).mount();
+        pagination: false
+	} ).mount();
+    var primarySlider = new Splide( '#primary-slider', {
+		type       : 'fade',
+		heightRatio: 0.5,
+		pagination : false,
+		arrows     : false,
+		cover      : true,
+    } );
+    primarySlider.sync( secondarySlider ).mount();
 }
