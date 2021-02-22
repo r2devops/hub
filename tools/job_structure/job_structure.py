778240b7a7b224d6550b531e332fdfa2ed157388
coconux <aurelien@go2scale.com>
2021-02-22 14:37:20 +0100

    Move variable to a config class

--- tools/job_structure/job_structure.py
+++ tools/job_structure/job_structure.py
@@ -5,15 +5,9 @@ import logging
 import sys
 import yaml
 
-# Job variables
-JOBS_DIR = "jobs"
-TOOLS_DIR = "tools"
-JOB_TEMPLATE_DIR = "job_template"
-JOB_DIR = "job_name"
-JOB_YAML = "job.yml"
-LOGFILE_NAME = os.getenv("JOB_LOGFILE")
-EXIT_SUCCESS = 0
-EXIT_FAILURE = 1
+from tools.utils.utils import Config
+
+utils = Config()
 
 def check_job_yaml(job):
     """Verify the content of job.yaml for every job
@@ -32,18 +26,18 @@ def check_job_yaml(job):
     """
     ret = EXIT_SUCCESS
 
-    with open(f"{TOOLS_DIR}/{JOB_TEMPLATE_DIR}/{JOB_DIR}/{JOB_YAML}", "r") as template_yml, open(f"{JOBS_DIR}/{job}/{JOB_YAML}", "r") as job_yml:
+    with open(f"{utils.TOOLS_DIR}/{utils.JOB_TEMPLATE_DIR}/{utils.JOB_DIR}/{utils.JOB_YAML}", "r") as template_yml, open(f"{utils.JOBS_DIR}/{job}/{utils.JOB_YAML}", "r") as job_yml:
         template_content = yaml.load(template_yml, Loader=yaml.FullLoader)
         job_content = yaml.load(job_yml, Loader=yaml.FullLoader)
 
-        logging.info(f"Checking the content of {JOB_YAML} in job {job}")
+        logging.info(f"Checking the content of {utils.JOB_YAML} in job {job}")
         diff = set(template_content.keys()) - set(job_content.keys())
         if len(diff) > 0:
             for item in diff:
-                logging.error(f"Key {item} in {JOB_YAML} of job {job} is missing")
-            ret = EXIT_FAILURE
+                logging.error(f"Key {item} in {utils.JOB_YAML} of job {job} is missing")
+            ret = utils.EXIT_FAILURE
         else:
-            logging.info(f"{JOB_YAML} for job {job} is complete")
+            logging.info(f"{utils.JOB_YAML} for job {job} is complete")
     return ret
 
 def check_directory_structure(template_structure, job):
@@ -67,7 +61,7 @@ def check_directory_structure(template_structure, job):
     # Change "job_name" in template for the actual job name for comparison
     template_structure_tmp = [obj.replace("job_name", f"{job}") for obj in template_structure]
 
-    job_structure = [os.path.join(parent, name) for (parent, subdirs, files) in os.walk(f"{JOBS_DIR}/{job}") for name in files + subdirs]
+    job_structure = [os.path.join(parent, name) for (parent, subdirs, files) in os.walk(f"{utils.JOBS_DIR}/{job}") for name in files + subdirs]
     # Adding the job directory
     job_structure.append(f"{job}")
 
@@ -84,13 +78,13 @@ def check_directory_structure(template_structure, job):
             if len(os.listdir(item)) == 0:
                 logging.error(f"Directory {item} for job {job} is empty")
 
-    ret = EXIT_SUCCESS
+    ret = utils.EXIT_SUCCESS
     if len(set(template_structure_tmp).intersection(job_structure)) != len(template_structure_tmp):
         # Not every file and directories in template_structure_tmp matched the job structure
         logging.error(f"Job structure of {job} does not match the template:")
         for item in set(template_structure_tmp) - set(template_structure_tmp).intersection(job_structure):
             logging.error(f"\tFile/directory missing: {item}")
-        ret = EXIT_FAILURE
+        ret = utils.EXIT_FAILURE
     else:
         logging.info(f"Job structure of {job} matches the template")
     return ret
@@ -112,22 +106,22 @@ if __name__ == "__main__":
         level=logging.INFO,
         format="%(asctime)s [%(levelname)s] %(message)s",
         handlers=[
-            logging.FileHandler(LOGFILE_NAME),
+            logging.FileHandler(utils.LOGFILE_NAME),
             logging.StreamHandler()
         ]
     )
 
     # Iterate over every directories in jobs directory to create their job.md for the documentation
-    jobs = os.listdir(JOBS_DIR)
-    template_structure = [os.path.join(parent, name) for (parent, subdirs, files) in os.walk(f"{TOOLS_DIR}/{JOB_TEMPLATE_DIR}") for name in files + subdirs]
+    jobs = os.listdir(utils.JOBS_DIR)
+    template_structure = [os.path.join(parent, name) for (parent, subdirs, files) in os.walk(f"{utils.TOOLS_DIR}/{utils.JOB_TEMPLATE_DIR}") for name in files + subdirs]
     # Clear the first 2 directories
     template_structure = [obj[obj.find('/') + 1:] for obj in template_structure]
     template_structure = [obj[obj.find('/') + 1:] for obj in template_structure]
 
-    ret = EXIT_SUCCESS
+    ret = utils.EXIT_SUCCESS
     for job in jobs:
-        if check_directory_structure(template_structure, job) != EXIT_SUCCESS:
-            ret = EXIT_FAILURE
-        elif check_job_yaml(job) != EXIT_SUCCESS:
-            ret = EXIT_FAILURE
+        if check_directory_structure(template_structure, job) != utils.EXIT_SUCCESS:
+            ret = utils.EXIT_FAILURE
+        elif check_job_yaml(job) != utils.EXIT_SUCCESS:
+            ret = utils.EXIT_FAILURE
     sys.exit(ret)
---
+++ tools/utils/utils.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+
+import os
+import logging
+import sys
+import yaml
+
+class Config:
+    # Job variables
+    JOBS_DIR = "jobs"
+    TOOLS_DIR = "tools"
+    JOB_TEMPLATE_DIR = "job_template"
+    JOB_DIR = "job_name"
+    JOB_YAML = "job.yml"
+    JOB_DESCRIPTION_FILE = "README.md"
+    JOB_METADATA_FILE = "job.yml"
+    JOBS_EXTENSION = ".yml"
+    JOB_CHANGELOG_DIR = "versions"
+    # Directory name to use for the jobs screenshot
+    SCREENSHOTS_DIR = "screenshots"
+    ISSUES_LIMIT = 5
+
+    # Logging
+    LOGFILE_NAME = os.getenv("JOB_LOGFILE")
+    EXIT_SUCCESS = 0
+    EXIT_FAILURE = 1
+
+    # Mkdocs
+    MKDOCS_DIR = "docs"
+    MKDOCS_PLACEHOLDER_FILE = "placeholder.md"
+    MARKDOWN_EXTENSION = ".md"
+    # Path to images used for the built job documentation
+    MKDOCS_DIR_JOBS_IMAGES = "images/jobs"
+
+    # Requests variable
+    GITLAB_BASE_URL = "https://gitlab.com/"
+    GITLAB_API_URL = "https://gitlab.com/api/v4/"
+    R2DEVOPS_URL = "https://jobs.r2devops.io/"
+    PROJECT_NAME = "r2devops/hub"
+    JOBS_SCOPE_LABEL = "Jobs::"
+
+    # Templates variables
+    BUILDER_DIR = "tools/builder"
+    TEMPLATE_DIR = "templates"
+    TEMPLATE_INDEX = "index.md.j2"
+    TEMPLATE_DOC = "job_documentation.md.j2"
+    TEMPLATE_PLACEHOLDER = "placeholder.md.j2"
+    TEMPLATE_LICENSE_DIR = "licenses"
+    INDEX_FILE = "index.md"
+
+    # List of stages
+    INDEX = {
+        "static_tests": {"name":"Static_tests","icon":"🔎","content":[], "description":"Static testing of repository files"},
+        "build": {"name":"Build","icon":"🧱","content":[], "description":"Building and packaging of software"},
+        "dynamic_tests": {"name":"Dynamic_tests","icon":"🔥","content":[], "description":"Dynamic testing of a running version of the software"},
+        "provision": {"name":"Provision","icon":"🛠","content":[], "description":"Preparation of the software infrastructure"},
+        "review": {"name":"Review","icon":"👌","content":[], "description":"Deployment of the software in an isolated review environment"},
+        "release": {"name":"Release","icon":"🏷","content":[], "description":"Releasing and tagging of the software"},
+        "deploy": {"name":"Deploy","icon":"🚀","content":[], "description":"Deployment of the software on environments"},
+        "others": {"name":"Others","icon":"🦄","content":[], "description":"All other magic jobs not included in previous stages"}
+    }
+
+
+
+
+if __name__ == "__main__":
+    """Main function, iterating over job structures to compare to the tempale one
+
+    Returns
+    -------
+    0
+        In case nothing was mismatched
+    Number
+        Number of jobs that doesn't match the template
+    """
+    # Setup logging
+    logging.basicConfig(
+        encoding="utf-8",
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        handlers=[
+            logging.FileHandler(LOGFILE_NAME),
+            logging.StreamHandler()
+        ]
+    )
